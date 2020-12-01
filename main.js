27093ad1cdd3809e3c588486267fd62dc65f318d
ryo264417 <75295424+ryo264417@users.noreply.github.com>
2020-12-02 04:42:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -43,15 +43,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     const model = await modelPromise
 
     const prediction = tf.tidy(() => {
-      let input1 = tf.fromPixels(ctx1.getImageData(0, 0, 150, 150)).resizeNearestNeighbor([100,100]);
+      let input1 = tf.fromPixels(ctx1.getImageData(0, 0, 150, 150)).resizeNearestNeighbor([224,224]);
       input1 = tf.cast(input1, 'float32').div(tf.scalar(255));
       input1 = input1.expandDims();
 
-      let input2 = tf.fromPixels(ctx2.getImageData(0, 0, 150, 150)).resizeNearestNeighbor([100,100]);
+      let input2 = tf.fromPixels(ctx2.getImageData(0, 0, 150, 150)).resizeNearestNeighbor([224,224);
       input2 = tf.cast(input2, 'float32').div(tf.scalar(255));
       input2 = input2.expandDims();
 
-      let input3 = tf.fromPixels(ctx3.getImageData(0, 0, 150, 150)).resizeNearestNeighbor([100,100]);
+      let input3 = tf.fromPixels(ctx3.getImageData(0, 0, 150, 150)).resizeNearestNeighbor([224,224]);
       input3 = tf.cast(input3, 'float32').div(tf.scalar(255));
       input3 = input3.expandDims();
       return model.predict([input1, input2, input3]).dataSync()[0];
